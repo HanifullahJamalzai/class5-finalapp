01ff4393098c5f8efea7242ed85d392c3bc80924
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2023-03-04 17:08:32 +0430

    features added

--- app/Http/Controllers/landing/LandingController.php
+++ app/Http/Controllers/landing/LandingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\landing;
 
 use App\Http\Controllers\Controller;
 use App\Models\Property;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
@@ -11,7 +12,8 @@ class LandingController extends Controller
     public function index()
     {
         return view('landing.index')
-                ->with('sliders', Property::limit(4)->orderBy('id','desc')->get());
+                ->with('sliders', Property::limit(4)->orderBy('id','desc')->get())
+                ->with('features', Service::limit(6)->orderBy('id','desc')->get());
     }
 
     public function property($id, $property)
--- resources/views/landing/layouts/partials/aside.blade.php
+++ resources/views/landing/layouts/partials/aside.blade.php
@@ -26,52 +26,7 @@
                     </div>
                 </li>
             @endforeach
-           {{-- <li style="background-image: url({{ asset('landing_assets/images/img_bg_2.jpg') }});">
-               <div class="container-fluid">
-                   <div class="row">
-                       <div class="col-md-4 col-md-offset-4 col-md-pull-4 js-fullheight slider-text">
-                           <div class="slider-text-inner">
-                               <div class="desc">
-                                   <span class="status">Rent</span>
-                                   <h1>New House in Canada, UK</h1>
-                                    <h2 class="price">$2000/mos</h2>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="details">
-                                        <span>2000 ft sq</span>
-                                        <span>4 Bedrooms</span>
-                                        <span>3 Bathrooms</span>
-                                        <span>2 Garage</span>
-                                    </p>
-                                    <p><a class="btn btn-primary btn-lg" href="#">Learn More</a></p>
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </li>
-           <li style="background-image: url({{ asset('landing_assets/images/img_bg_3.jpg') }});">
-               <div class="container-fluid">
-                   <div class="row">
-                       <div class="col-md-4 col-md-offset-4 col-md-pull-4 js-fullheight slider-text">
-                           <div class="slider-text-inner">
-                               <div class="desc">
-                                   <span class="status">Sale</span>
-                                   <h1>New House in Canada, UK</h1>
-                                    <h2 class="price">$4,000.00</h2>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="details">
-                                        <span>2000 ft sq</span>
-                                        <span>4 Bedrooms</span>
-                                        <span>3 Bathrooms</span>
-                                        <span>2 Garage</span>
-                                    </p>
-                                    <p><a class="btn btn-primary btn-lg" href="#">Learn More</a></p>
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </li>		   	 --}}
+           
           </ul>
       </div>
 </aside>
